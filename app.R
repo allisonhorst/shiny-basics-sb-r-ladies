908d29f5cee0c2ef745f9274c2701e0241c69d58
Allison Horst <ahorst@ucsb.edu>
2019-10-27 12:13:28 -0700

    update to match teaching key

--- app.R
+++ app.R
@@ -2,66 +2,64 @@
 # SB R-Ladies 2019-10-30
 # Prepared by Allison Horst
 
+library(tidyverse)
 library(shiny)
 library(shinythemes)
-library(tidyverse)
-library(glue)
 
 spooky <- read_csv("spooky_data.csv")
 
-# Create the user interface:
 ui <- fluidPage(
   theme = shinytheme("slate"),
-  titlePanel("A spooky Shiny app!"),
+  titlePanel("I am adding a title!"),
   sidebarLayout(
     sidebarPanel("put my widgets here",
                  selectInput(inputId = "state_select",
-                              label = "Choose state:",
-                              choices = unique(spooky$state)
+                             label = "Choose a state:",
+                             choices = unique(spooky$state)
                  ),
                  radioButtons(inputId = "region_select",
-                              label = "Choose a region:",
+                              label = "Choose region:",
                               choices = unique(spooky$region_us_census))
-                 ),
-    mainPanel(h1("Happy Halloween!"),
+    ),
+    mainPanel("put my outputs here",
               p("State's top candies:"),
               tableOutput(outputId = "candy_table"),
-              p("Region's top costumes:"),
+              p("Region’s top costumes:"),
               plotOutput(outputId = "costume_graph")
-              )
-)
+    )
+  )
 )
 
-# Create the server:
 server <- function(input, output) {
 
-state_candy <- reactive({
+# Create reactive object state_candy that changes based on state_select widget selection
+  state_candy <- reactive({
     spooky %>%
       filter(state == input$state_select) %>%
-    select(candy, pounds_candy_sold)
+      select(candy, pounds_candy_sold)
   })
 
-region_costume <- reactive({
-  spooky %>%
-    filter(region_us_census == input$region_select) %>%
-    count(costume, rank)
+# Render a reactive table that uses state_candy reactive object (and note the parentheses after state_candy -- do that if calling a reactive object!)
+  output$candy_table <- renderTable({
+    state_candy()
+  })
 
-})
+# Create a reactive object region_costume that only contains observations for the region selected in the region_select widget, then finds counts by costume & rank
+  region_costume <- reactive({
+    spooky %>%
+      filter(region_us_census == input$region_select) %>%
+      count(costume, rank)
+  })
 
-output$candy_table <- renderTable({
-  state_candy()
-})
-
-output$costume_graph <- renderPlot({
-  ggplot(region_costume(), aes(x = costume, y = n)) +
-    geom_col(aes(fill = rank)) +
-    coord_flip() +
-    scale_fill_manual(values = c("black","purple","orange")) +
-    theme_minimal()
-})
-
+# Render a reactive graph with the counts of top costumes for each region:
+  output$costume_graph <- renderPlot({
+    ggplot(region_costume(), aes(x = costume, y = n)) +
+      geom_col(aes(fill = rank)) +
+      coord_flip() +
+      scale_fill_manual(values = c("black","purple","orange")) +
+      theme_minimal()
+  })
 }
 
-# Combine them into an app:
+# Put them together!
 shinyApp(ui = ui, server = server)
-
